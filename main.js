559e36492371262133766c0eb1c56a381131ab08
1eandro <leandro.rfismael2492@gmail.com>
2023-12-06 22:54:22 -0300

    primer

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
+const auto1 = "Peugeot";
+const auto2 = " Citroen";
+const auto3 = "Chevrolet"; 
 
-const auto = parseInt(prompt("Elegi tu auto ideal para vos: \n 1=peugeot \n 2=toyota \n 3=ford"))
+alert ("Elegi tu marca preferida de auto \n1. Peugeot\n2. Citroen\n3. Chevrolet");
 
-const car1 = prompt("peugeot");
-const car2 = prompt ("toyota");
-const car3 = prompt("ford");
+let elegiTuAuto = parseInt(prompt("Elegi tu marca preferida"))
 
-let finalizacion = prompt("gracias por comprar tu marca de autos preferida")
+while (elegiTuAuto > 3){
+        alert ("por favor ingrese un marca valida")
+    elegiTuAuto = parseInt(prompt("ingresar una marca valida"))
+}
 
-switch (auto){
-    case 1:
-        
-        break;
-    case 2:
+if (elegiTuAuto == 1){
+    alert("Gracias por tu solicitud para tu compra de tu" + auto1)
 
+    let peugeot ={
+    marca:'peugeot',
+    modelo: '308',
+    peso: '1100',
+    color: 'negro'
+    
+    }
+    alert('elegiste tu peugeot 308')
+    console.log('peugeot', peugeot)
+}
 
+else if(elegiTuAuto == 2){
+    alert("Gracias por tu solicitud para tu compra de tu" + auto2)
 
+    let citroen = {
+    marca: 'citroen',
+    modelo: 'ds3',
+    peso: '1200',
+    color: 'blanco'
+    
+    }
+    
+    alert('elegiste tu citroen ds3')
+    console.log('citroen', citroen)
+}
+
+else if ( elegiTuAuto == 3){
+    alert("Gracias por tu solicitud para tu compra de tu" + auto3)
+
+    let chevrolet = {
+        marca:'chevrolet',
+        modelo: 'onix',
+        peso: '11800',
+        color: 'plata'
+    
+    }
+    alert('elegiste tu chevrolet onix')
+    console.log('chevrolet', chevrolet)
 }
