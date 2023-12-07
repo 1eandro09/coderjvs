d1349baa490f7f7a2e389a9496fd103bab08b3a1
1eandro <leandro.rfismael2492@gmail.com>
2023-12-06 22:45:15 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const auto = parseInt(prompt("Elegi tu auto ideal para vos: \n 1=peugeot \n 2=toyota \n 3=ford"))
+
+const car1 = prompt("peugeot");
+const car2 = prompt ("toyota");
+const car3 = prompt("ford");
+
+let finalizacion = prompt("gracias por comprar tu marca de autos preferida")
+
+switch (auto){
+    case 1:
+        
+        break;
+    case 2:
+
+
+
+}
